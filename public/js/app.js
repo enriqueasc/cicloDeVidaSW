cf92b408b14ac2addfedc540ae5219848113a5b2
anaarteagar <ana.rivera@uteq.edu.mx>
2024-02-02 15:54:22 -0600

    Inicio

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,10 @@
 if (navigator.serviceWorker) {
     navigator.serviceWorker.register('/sw.js')
+    // .then(reg => {
+    //     setTimeout(() => {
+    //         reg.sync.register('enviar-fotos')
+    //         console.log('Se enviaron las fotos al server')
+    //     }, 3000)
+    // })
+    // .catch(err => console.log(err))
 }
